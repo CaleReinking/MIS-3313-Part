a5dcae6418b0175567d66ae2552e4a62f9803b73
rein0004 <rein0004@PCB-LAB360-06>
2020-01-22 12:54:13 -0600

    Created variables

--- In Class Examples/Variables/Program.cs
+++ In Class Examples/Variables/Program.cs
@@ -11,6 +11,26 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create a string named firstName
+            string firstName;
+
+            //Assign a value to firstName Cale 
+            firstName = "Cale";
+
+
+            //Create an int named favoriteNumber and assign your fav num to it 
+            int favoriteNumber = 8;
+
+            double crazyNumber = 7.64732846 * favoriteNumber;
+
+            string lastName = "Reinking";
+            string fullName = firstName + "" + lastName;
+
+
+            Console.WriteLine(fullName + "has the favorite number of" + favoriteNumber
+                + "Which equates to a crazy number" + crazyNumber);
+            
+            Console.ReadKey();
         }
     }
 }
