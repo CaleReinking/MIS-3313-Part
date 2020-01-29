42c7d341d9d0f74f4380d572e66bbbf0b1851b9c
rein0004 <rein0004@PCB-LAB360-06>
2020-01-29 12:52:55 -0600

    Grinding

---
+++ In Class Part 1/In Class Part 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Class_Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Heads or Tails >>");
+            string userGuessAsString = Console.ReadLine();
+
+            int userGuess;
+
+            if (userGuessAsString == "Heads")
+            {
+                userGuess = 'Heads';
+            }
+            else
+            {
+                userGuess = 2 ;
+
+                    }
+            Random rand = new Random();
+
+            int randomNumber = rand.Next(1, 2);
+
+
+           
+
+
+
+
+              Console.ReadKey();
+
+        }
+    }
+}
