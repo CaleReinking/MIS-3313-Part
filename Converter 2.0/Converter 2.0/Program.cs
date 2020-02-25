c3d0ed0880ed71d830aeff20083e900ce33f7787
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-02-24 21:36:23 -0800

    Got it

---
+++ Converter 2.0/Converter 2.0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Converter_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a sentence");
+            string sentence = Console.ReadLine().ToUpper();
+
+            sentence = sentence.Replace('A', '@');
+            sentence = sentence.Replace('E', '3');
+            sentence = sentence.Replace("H", "|-|");
+            sentence = sentence.Replace('S', '$');
+            sentence = sentence.Replace('T', '7');
+            sentence = sentence.Replace("U", "|_|");
+
+            char last = sentence[sentence.Length-1];
+            if (last != '!')
+            {
+                Console.WriteLine($"{sentence}!");
+            }
+            else
+            {
+                Console.WriteLine(sentence);
+            }
+            Console.ReadKey();
+        }
+    }
+}
