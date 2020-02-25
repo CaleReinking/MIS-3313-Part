4dc4ac3fd266e5ec8f314964fd8f35ae2406e6ef
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-02-24 19:54:16 -0800

    tried

---
+++ GPA Calc 2.0/GPA Calc 2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GPA_Calc_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Waiting to see if there is a shorter way
+        }
+    }
+}
