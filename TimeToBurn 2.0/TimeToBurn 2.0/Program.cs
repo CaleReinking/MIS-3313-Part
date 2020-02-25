61e020464fd94f5f0700b290dbbb2ce69785671f
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-02-24 20:43:27 -0800

    Got it

---
+++ TimeToBurn 2.0/TimeToBurn 2.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TimeToBurn_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double calories;
+            Console.WriteLine("How many calories do you wish to burn?");
+            calories = Convert.ToDouble(Console.ReadLine());
+
+            double caloriestoburn = 3.7;
+          double caloriesburned = 0;
+            int minute = 1;
+            while (calories>caloriesburned)
+            {
+                caloriesburned = (minute * caloriestoburn);
+                Console.WriteLine($"After minute {minute} you have burnt {caloriesburned} .");
+                minute += 1;
+            }
+            Console.ReadKey();
+           
+        }
+    }
+}
