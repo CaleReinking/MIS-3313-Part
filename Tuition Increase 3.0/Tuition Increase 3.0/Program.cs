ab5299152fae2794bbda09b96d3c22fdeb5746c9
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-02-25 18:39:04 -0800

    Murked it

---
+++ Tuition Increase 3.0/Tuition Increase 3.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tuition_Increase_3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double tuition = 12000;
+            double rate = .05;
+            double year = 7;
+            for (int i = 1; i < year+1; i++)
+            {
+                 tuition += tuition * rate;
+                Console.WriteLine($"In year {i} your tuition will be {tuition.ToString("C2")}");
+                   
+
+            }
+        }
+    }
+}
