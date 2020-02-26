8bd1116bf6c9e41fcde4add4d8fbb3aae3433b81
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-02-25 19:52:14 -0800

    Push

---
+++ Accumulator2.0/Accumulator2.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Accumulator2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double multiplier = 1;
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+                multiplier *= i;
+            }
+            Console.WriteLine($"The sum is {sum}");
+            Console.WriteLine($"and multiplied is {multiplier.ToString("N0")}");
+        }
+    }
+}
