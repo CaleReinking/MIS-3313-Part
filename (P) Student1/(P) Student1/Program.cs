6eb52df73fab9a6895b6f000f95a1ee3336d26a6
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-04-06 12:55:25 -0700

    Commited

---
+++ (P) Student1/(P) Student1/Program.cs
@@ -0,0 +1,61 @@
+﻿// Cale J Reinking, worked with Micah and Shelby 
+using System;
+using System.Collections.Generic;
+
+namespace _P__Student1
+{
+    class Student1
+    {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> CourseNames { get; set; }
+        public List<string> CourseGrades { get; set; }
+
+        public Student1()
+        {
+            FirstName = "";
+            LastName = "";
+            List<string> CourseNames = new List<string>();
+            List<string> CourseGrades = new List<string>();
+
+        }
+        public Student1(string first, string second)
+        {
+            first = FirstName;
+            second = LastName;
+        }
+        public double CalculateGPA()
+        {
+            double points = 0;
+            double totalpoints = 0;
+
+            foreach (var item in CourseGrades)
+            {
+                if (item == "A")
+                {
+                    points = 4 * 3;
+                }
+                else if (item == "B")
+                {
+                    points = 3 * 3;
+                }
+                else if (item == "C")
+                {
+                    points = 2 * 3;
+                }
+                else if (item == "D")
+                {
+                    points = 1 * 3;
+                }
+                else
+                {
+                    points = 0;
+                }
+
+                totalpoints += points;
+            }
+            return totalpoints / CourseGrades.Count;
+        }
+    }
+}
---
+++ (P) Student1/(P) Student1/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P__Student1
+{
+    class Student
+    {
+        static void Main(string[] args)
+        {
+            Student1 stud1 = new Student1();
+            stud1.FirstName = "Cale";
+            stud1.LastName = "Reinking";
+            stud1.CourseGrades.Add("A");
+            stud1.CourseNames.Add("Programming by AA");
+            stud1.CourseGrades.Add("B");
+            stud1.CourseNames.Add("AAs harder programming class");
+            stud1.CalculateGPA();
+
+            Console.ReadKey();
+        }
+    }
+}
