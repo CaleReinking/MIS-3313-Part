6b336a9f648d1cf8f282c51a85b18abe91732762
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-04-09 12:27:46 -0700

    Got it

---
+++ P Classes 2 Toys/P Classes 2 Toys/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P_Classes_2_Toys
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        private string Aisle { get; set; }
+        public Toy()
+        {
+            Manufacturer = "";
+            Name = "";
+            Price = 0;
+            Aisle = "";
+
+        }
+        public string GetAisle()
+        {
+            return Manufacturer.First().ToString().ToUpper();
+        }
+        public string GetAisle2()
+        {
+            Random rand = new Random();
+            int randomnumber = rand.Next(1, 24);
+            return randomnumber.ToString();
+        }
+    }
+}
---
+++ P Classes 2 Toys/P Classes 2 Toys/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+// Collaborated with Meri and Ben
+namespace P_Classes_2_Toys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toy Toy1 = new Toy();
+
+            Toy1.Manufacturer = "Lego";
+            Toy1.Name = "Lego Fan";
+            Toy1.Price = 9.99;
+            Toy1.GetAisle();
+            Toy1.GetAisle2();
+
+            Console.WriteLine($"The toy manufacturer is {Toy1.Manufacturer}.");
+            Console.WriteLine($"The toy name is {Toy1.Name}.");
+            Console.WriteLine($"The price of the toy is {Toy1.Price}.");
+            Console.WriteLine($"The toy is located at aisle {Toy1.GetAisle()} {Toy1.GetAisle2()}.");
+
+            Console.ReadKey
+        }
+    }
+}
