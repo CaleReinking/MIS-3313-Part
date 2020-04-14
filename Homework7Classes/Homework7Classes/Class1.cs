10b45cf8ac434a461b03419b4d9f9fccf7329b4c
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-04-14 14:42:18 -0700

    Finished

---
+++ Homework7Classes/Homework7Classes/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7Classes
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Student stud1 = new Student();
+            stud1.FirstName = "Ben";
+            stud1.LastName = "Henke";
+            stud1.StudenID = 1800;
+            stud1.ExamGrades = new List<double>() { 90, 90, 100, 90, 90 };
+            stud1.FinalExamGrades = new List<double>() { 90, 10, 52, 33, 10 };
+            stud1.HomeworkGrades = new List<double>() { 90, 69, 79, 99, 59 };
+            stud1.ParticipationGrades = new List<double>() { 10, 95, 10, 95, 10 };
+            stud1.QuizGrades = new List<double>() { 97, 95, 90, 65, 75 };
+           
+            Console.WriteLine(stud1);
+            Console.WriteLine("Exam Average is {0}", stud1.CalculateExamAverage());
+            Console.WriteLine("Final Exam Average is {0}", stud1.CalculateFinalExamAverage());
+            Console.WriteLine("Homework Average is {0}", stud1.CalculateHomeworkAverage());
+            Console.WriteLine("Participation Average is {0}", stud1.CalculateParticipationAverage());
+            Console.WriteLine("Quiz Average is {0}", stud1.CalculateQuizAverage());
+            Console.WriteLine();
+
+            Student stud2 = new Student();
+            stud2.FirstName = "Cale";
+            stud2.LastName = "Reinking";
+            stud2.StudenID = 1801;
+            stud2.ExamGrades = new List<double>() { 99, 91, 100, 99, 99 };
+            stud2.FinalExamGrades = new List<double>() { 99, 100, 52, 33, 100 };
+            stud2.HomeworkGrades = new List<double>() { 99, 79, 69, 59, 49 };
+            stud2.ParticipationGrades = new List<double>() { 100, 90, 100, 90, 100 };
+            stud2.QuizGrades = new List<double>() { 87, 95, 100, 75, 65 };
+
+            Console.WriteLine(stud2);
+            Console.WriteLine("Exam Average is {0}", stud2.CalculateExamAverage());
+            Console.WriteLine("Final Exam Average is {0}", stud2.CalculateFinalExamAverage());
+            Console.WriteLine("Homework Average is {0}", stud2.CalculateHomeworkAverage());
+            Console.WriteLine("Participation Average is {0}", stud2.CalculateParticipationAverage());
+            Console.WriteLine("Quiz Average is {0}", stud2.CalculateQuizAverage());
+            Console.WriteLine();
+
+            Student stud3 = new Student();
+            stud3.FirstName = "Meri";
+            stud3.LastName = "Santonomero";
+            stud3.StudenID = 1802;
+            stud3.ExamGrades = new List<double>() { 90, 97, 90, 90, 90 };
+            stud3.FinalExamGrades = new List<double>() { 90, 100, 52, 93, 100 };
+            stud3.HomeworkGrades = new List<double>() { 97, 69, 69, 50, 99 };
+            stud3.ParticipationGrades = new List<double>() { 24, 90, 10, 90, 10 };
+            stud3.QuizGrades = new List<double>() { 97, 95, 98, 75, 98 };
+
+            Console.WriteLine(stud3);
+            Console.WriteLine("Exam Average is {0}", stud3.CalculateExamAverage());
+            Console.WriteLine("Final Exam Average is {0}", stud3.CalculateFinalExamAverage());
+            Console.WriteLine("Homework Average is {0}", stud3.CalculateHomeworkAverage());
+            Console.WriteLine("Participation Average is {0}", stud3.CalculateParticipationAverage());
+            Console.WriteLine("Quiz Average is {0}", stud3.CalculateQuizAverage());
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework7Classes/Homework7Classes/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+//Cale J Reinking, worked with Derek Smith, Ben Henke and Meri Santonomero
+
+namespace Homework7Classes
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int StudenID { get; set; }
+        public List <double> ExamGrades { get; set; } //Attribute List for exam grades
+        public List<double> FinalExamGrades { get; set; }
+        public List<double> HomeworkGrades { get; set; }
+        public List<double> ParticipationGrades { get; set; }
+        public List<double> QuizGrades { get; set; }
+
+        public Student() // Constructor: Gives all of the attributes above a value
+        {
+            FirstName = "";
+            LastName = "";
+            StudenID = 0;
+            ExamGrades = new List<double>();
+            FinalExamGrades = new List<double>();
+            HomeworkGrades = new List<double>();
+            ParticipationGrades = new List<double>();
+            QuizGrades = new List<double>();
+        }
+        public Student(int studentId)
+        {
+            StudenID = studentId;
+        }
+        public Student(string firstname, string lastname, int studentID)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            StudenID = studentID; //This is for the Console.WriteLine(Stud1) on the other page 
+            //Set the names equal and in the method below, you write out the correct output
+        }
+        public override string ToString()
+        {
+            return LastName + "," + FirstName + "(" + StudenID + ")"; // This is what writes out their info
+        }
+        public double CalculateExamAverage()
+        {
+            double avg = 0;
+            foreach (double score in ExamGrades)
+            {
+                avg += score;
+            }
+            avg /= ExamGrades.Count;
+            return avg;
+        }
+        public double CalculateFinalExamAverage()
+        {
+            double avg = 0;
+            foreach (double score in FinalExamGrades)
+            {
+                avg += score;
+            }
+            avg /= FinalExamGrades.Count;
+            return avg;
+        }
+        public double CalculateHomeworkAverage()
+        {
+            double avg = 0;
+            foreach (double score in HomeworkGrades)
+            {
+                avg += score;
+            }
+            avg /= HomeworkGrades.Count;
+            return avg;
+        }
+        public double CalculateParticipationAverage()
+        {
+            double avg = 0;
+            foreach (double score in ParticipationGrades)
+            {
+                avg += score;
+            }
+            avg /= ParticipationGrades.Count;
+            return avg;
+        }
+        public double CalculateQuizAverage()
+        {
+            double avg = 0;
+            foreach (double score in QuizGrades)
+            {
+                avg += score;
+            }
+            avg /= QuizGrades.Count;
+            return avg;
+        }
+    }
+}
